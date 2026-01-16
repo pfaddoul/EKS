4fa8d121524f3f46b1a9ff34ca004c157c9449b9
patfaddoul <p.faddoul.ext@tradias.de>
2026-01-16 12:56:46 +0000

    add main and variables

--- main.tf
+++ main.tf
@@ -1,69 +1,50 @@
-# Resource-1: Create VPC
-resource "aws_vpc" "vpc-dev" {
-  cidr_block = "10.0.0.0/16"
+# --- VPC ---
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = var.vpc_cidr
+  enable_dns_hostnames = true
   tags = {
-    "Name" = "vpc-dev"
+    Name = var.vpc_name
   }
 }
 
-# Resource-2: Create Subnets
-resource "aws_subnet" "vpc-dev-public-subnet-1" {
-  vpc_id                  = aws_vpc.vpc-dev.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+  tags = {
+    Name = "IGW-${var.vpc_name}"
+  }
+}
+resource "aws_subnet" "eks_subnet_public" {
+  count = 1
+  vpc_id            = aws_vpc.eks_vpc.id
   map_public_ip_on_launch = true
-}
-
-# Resource-3: Internet Gateway
-resource "aws_internet_gateway" "vpc-dev-igw" {
-  vpc_id = aws_vpc.vpc-dev.id
-}
-
-# Resource-4: Create Route Table
-resource "aws_route_table" "vpc-dev-public-route-table" {
-  vpc_id = aws_vpc.vpc-dev.id
-}
-
-# Resource-5: Create Route in Route Table for Internet Access
-resource "aws_route" "vpc-dev-public-route" {
-  route_table_id         = aws_route_table.vpc-dev-public-route-table.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.vpc-dev-igw.id
-}
-
-# Resource-6: Associate the Route Table with the Subnet
-resource "aws_route_table_association" "vpc-dev-public-route-table-associate" {
-  route_table_id = aws_route_table.vpc-dev-public-route-table.id
-  subnet_id      = aws_subnet.vpc-dev-public-subnet-1.id
-}
-
-# Resource-7: Create Security Group
-resource "aws_security_group" "dev-vpc-sg" {
-  name        = "dev-vpc-default-sg"
-  description = "Dev VPC Default Security Group"
-  vpc_id      = aws_vpc.vpc-dev.id
-
-  ingress {
-    description = "Allow Port 22"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  availability_zone = var.availability_zone[count.index % length(var.availability_zone)]
+  tags = {
+    Name = "Subnet-${count.index}"
   }
+}
 
-  ingress {
-    description = "Allow Port 80"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_subnet" "eks_subnet_private" {
+  count = 5
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  availability_zone = var.availability_zone[count.index % length(var.availability_zone)]
+  tags = {
+    Name = "Subnet-${count.index}"
   }
+}
+resource "aws_route_table" "k8s-acc" {
+  vpc_id = aws_vpc.k8s-acc.id
 
-  egress {
-    description = "Allow all IP and Ports Outbound"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.k8s-acc.id
   }
+}
+
+resource "aws_route_table_association" "k8s-acc" {
+  count = 2
+
+  subnet_id      = aws_subnet.k8s-acc[count.index].id
+  route_table_id = aws_route_table.k8s-acc.id
 }
--- variables.tf
+++ variables.tf
@@ -10,5 +10,24 @@ variable "node_count" {
 
 variable "vpc_cidr" {
   type = string
-  default = "10.100.0.0/16"
+  default = "10.0.0.0/16"
+}
+
+variable "node_instance_type" {
+  type = string
+  default = "t2.medium"
+}
+
+variable "vpc_name" {
+  type = string
+  default = "eks_vpc"  
+}
+
+variable "aws_subnet" {
+  type = string
+}
+
+variable "availability_zone"{
+  type = string
+  default = "us-east-1"
 }
